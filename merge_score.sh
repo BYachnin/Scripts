2dc49735e4d35474376d734116fc4a7dd630f150
Brahm Yachnin <brahm.yachnin@rutgers.edu>
2017-09-25 13:50:50 -0400

    Added code to merge_score.sh to deal with multiple .sc files and empty directories.

--- merge_score.sh
+++ merge_score.sh
@@ -8,14 +8,14 @@ fi
 firstfile=1
 
 for jobdir in */; do
-  if [ -e $jobdir*.sc ]; then
-    for scorefile in $jobdir*.sc; do
+  for scorefile in $jobdir*.sc; do
+    if [ -e $scorefile ]; then   
       if [ $firstfile = 1 ]; then
         cat $scorefile > score_mg.sc
         firstfile=0
       else
         tail -n +3 $scorefile >> score_mg.sc
       fi
-    done
-  fi
+	fi
+  done
 done
