830c0623b5f1ceaa3c6e59d11a284c998af64dab
Brahm Yachnin <brahm.yachnin@rutgers.edu>
2020-04-21 19:07:17 -0400

    cif support for pdb_seq_to_fas

--- pdb_seq_to_fas.py
+++ pdb_seq_to_fas.py
@@ -5,8 +5,8 @@ init("-ignore_unrecognized_res")
 seq_file = open("sequences.fas", "w")
 
 for pdb in sorted(os.listdir(os.getcwd())):
-  if pdb.endswith(".pdb"):
-    pose = pose_from_pdb(pdb)
+  if pdb.endswith(".pdb") or pdb.endswith(".cif"):
+    pose = pose_from_file(pdb)
     seq_file.write(">"+pdb+"\n")
     seq_file.write(pose.sequence()+"\n")
     continue
