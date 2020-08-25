cbcf9033708f9a604eb99093060797f96c8b5567
Brahm Yachnin <brahm.yachnin@rutgers.edu>
2020-08-25 15:41:44 -0400

    Add pyrosetta ipython profile

---
+++ pyrosetta_ipy_startup/loadModules.py
@@ -0,0 +1,38 @@
+# import system libraries
+import glob
+import os
+from importlib import reload
+
+# import install libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# import visterra libraries
+from utils import antibody_utils
+from utils import file_utils
+from utils import seq_utils
+from utils import protein_utils
+from utils import design_utils
+
+# import vis_rosetta libraries
+import vis_rosetta
+from vis_rosetta.core.Antibody import Antibody
+from vis_rosetta.core.Protein import Protein
+from vis_rosetta.core.ProteinGenerator import ProteinGenerator
+from vis_rosetta.core.JobDistributor import JobDistributor
+from vis_rosetta.filters.CompoundFilter import CompoundFilter
+from vis_rosetta.filters.FuzzyFilter import FuzzyFilter
+from vis_rosetta.IO.silent_file_manager import SilentFileReader, SilentFileWriter
+from vis_rosetta.movers import ab_movers
+from vis_rosetta.movers import design_movers
+from vis_rosetta.movers import geom_movers
+from vis_rosetta.movers import minimize_movers
+from vis_rosetta.select.selectors import Selector, CompoundSelector
+from vis_rosetta.protocols.protocol_parser import ProtocolManager
+from vis_rosetta.select import select_utils
+from vis_rosetta.tasks import task_operations
+from vis_rosetta.utils import pymol_utils
+
+vis_rosetta.start(mute_all=True)
